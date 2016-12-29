c4b6360ec9ead65c02006e97be4b5c7ce46dec5a
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 08:58:05 -0600

    marlin/sailfish: initial carbon bringup
    Change-Id: I33e512ffe3395a8263d22456e2fb37dba9b8bca8

---
+++ carbon_marlin.mk
@@ -0,0 +1,30 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+
+-include device/google/marlin/marlin/device-carbon.mk
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := carbon_marlin
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel XL
+TARGET_MANUFACTURER := HTC
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    BUILD_FINGERPRINT=google/marlin/marlin:8.0.0/OPR6.170623.012/4283428:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.0.0 OPR6.170623.012 4283428 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carbon.maintainer="Myself5"
---
+++ sailfish/BoardConfigCarbon.mk
@@ -0,0 +1,2 @@
+# BoardConfig for lineage_sailfish
+include device/google/marlin/BoardConfigCarbon.mk
