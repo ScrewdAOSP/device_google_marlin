a6102f22d41e4f7e37653098083bae0cdc8e35d3
Jake Weinstein <xboxlover360@gmail.com>
2017-05-08 20:25:34 -0400

    Marlin/Sailfish: increase hwui large cache height to reduce jank
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- device-common.mk
+++ device-common.mk
@@ -64,7 +64,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.text_small_cache_width=1024 \
     ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
+    ro.hwui.text_large_cache_height=2048
 
 # For android_filesystem_config.h
 PRODUCT_PACKAGES += fs_config_files \
